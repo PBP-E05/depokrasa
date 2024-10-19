217800ba4afd4c110040e8db24e4248b971d7b76
zerothy <zxuyagaming@gmail.com>
2024-10-19 10:58:23 +0700

    feat: implement wishlist

--- depokrasa/urls.py
+++ depokrasa/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls', namespace='authentication')),
+    path('user/', include('usermanagement.urls', namespace='usermanagement')),
     path('', include('main.urls')),
 ]
