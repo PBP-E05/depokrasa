9ca15628099b85c3f392f88fd97902008f79aae6
zerothy <zxuyagaming@gmail.com>
2024-10-27 15:13:28 +0700

    feat: news done

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, show_news_json, create_news_ajax, delete_news
+from main.views import show_main, show_news_json, create_news_ajax, delete_news, add_to_wishlist
 
 app_name = 'main'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('news-json/', show_news_json, name='show_news_json'),
     path('create-news-ajax/', create_news_ajax, name='create_news_ajax'),
     path('delete-news/<uuid:id>/', delete_news, name='delete_news'),
+    path('add-to-wishlist/', add_to_wishlist, name='add_to_wishlist'),
 ]
