4183ec317e6125d67efdc26f0db9f0080dd89c9a
Philip1715 <philiphalomoan17@gmail.com>
2024-10-27 22:17:01 +0700

    add ajax and edit main page

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import show_main, show_news_json, create_news_ajax, delete_news
-
+from . import views
 app_name = 'main'
 
 urlpatterns = [
@@ -8,4 +8,7 @@ urlpatterns = [
     path('news-json/', show_news_json, name='show_news_json'),
     path('create-news-ajax/', create_news_ajax, name='create_news_ajax'),
     path('delete-news/<uuid:id>/', delete_news, name='delete_news'),
+    path('add-restaurant/', views.add_restaurant, name='add_restaurant'),
+    path('show-template/', views.my_template_view, name='show_template'),
+    path('insert_data/', views.insert_restaurant_data, name='insert_data'),
 ]
